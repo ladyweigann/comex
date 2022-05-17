e61b103b4e95a4fa6279c5c12a0c2fe0efc4e4dc
Larissa Labanca <ladyweigann@gmail.com>
2022-05-17 14:15:45 -0300

    ordem alfabetica no output do relatorio de vendas por categoria

--- src/main/java/br/com/alura/comex/RelatorioVendasCategoria.java
+++ src/main/java/br/com/alura/comex/RelatorioVendasCategoria.java
@@ -3,9 +3,11 @@ package br.com.alura.comex;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.text.NumberFormat;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.stream.Collectors;
 
 public class RelatorioVendasCategoria {
@@ -15,11 +17,13 @@ public class RelatorioVendasCategoria {
 		Map<String, List<Pedido>> categorias = pedidos.stream()
 				.collect(Collectors.groupingBy(Pedido::getCategoria));
 		
-		categorias
+		categorias.entrySet().stream().sorted(Entry.comparingByKey()).collect(Collectors.toMap(Entry::getKey, Entry::getValue, (c1, c2) -> c1, LinkedHashMap::new))	//Ordem alfabética	
 		.forEach((categoria, pedido) -> {
 			System.out.printf("\nCATEGORIA: " + categoria 
 					+ "\nQUANTIDADE VENDIDA: " + pedido.stream().mapToInt(Pedido::getQuantidade).sum()
-					+ "\nMONTANTE: %s\n", NumberFormat.getCurrencyInstance(new Locale("pt", "BR")).format(pedido.stream().map(Pedido::getValorTotal).reduce(BigDecimal.ZERO, BigDecimal::add).setScale(2, RoundingMode.HALF_DOWN)));
+					+ "\nMONTANTE: %s\n", NumberFormat.getCurrencyInstance(new Locale("pt", "BR"))
+					.format(pedido.stream().map(Pedido::getValorTotal)
+					.reduce(BigDecimal.ZERO, BigDecimal::add).setScale(2, RoundingMode.HALF_DOWN)));
 		});
 		
 	}
