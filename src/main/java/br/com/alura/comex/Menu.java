1d1c22fb4e3570eed2b436667b4a5aef136c4154
Larissa Labanca <ladyweigann@gmail.com>
2022-05-23 14:06:35 -0300

    menu implementado sem uso de design patterns ou refatorações

---
+++ src/main/java/br/com/alura/comex/Menu.java
@@ -0,0 +1,9 @@
+
+package br.com.alura.comex;
+
+public class Menu {
+
+	public static void exibirMenu() {
+		System.out.println("Bem-vindo ao Comex\n\nDigite a opção correspondente ao tipo de arquivo que será lido: \n1 - CSV;\n2 - JSON;\n3 - XML");
+	}
+}
