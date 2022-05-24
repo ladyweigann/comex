8595b690012c92a8ef6176d60f4f495f47b7436a
Larissa Labanca <ladyweigann@gmail.com>
2022-05-24 14:03:52 -0300

    lógica retirada da classe main e inserida na classe menu

--- src/main/java/br/com/alura/comex/Main.java
+++ src/main/java/br/com/alura/comex/Main.java
@@ -1,68 +1,13 @@
 package br.com.alura.comex;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
-import br.com.alura.comex.processadores.ProcessadorDeCsv;
-import br.com.alura.comex.processadores.ProcessadorDeJson;
-import br.com.alura.comex.processadores.ProcessadorDeXml;
-import br.com.alura.comex.relatorios.RelatorioClientesFieis;
-import br.com.alura.comex.relatorios.RelatorioSintetico;
-import br.com.alura.comex.relatorios.RelatorioVendasCategoria;
-
 public class Main {
 
     public static void main(String[] args) throws Exception{
     	
-    	Scanner sc = new Scanner(System.in);
+    	
     	Menu.exibirMenu();
-    	int opt = sc.nextInt();
-    	System.out.println("Digite o nome do seu arquivo (sem extensão)");
-    	String nomeArquivo = sc.next();
-    	List<Pedido> pedidos = new ArrayList<>();
     	
-    	
-    	switch (opt) {
-		case 1: {
-			pedidos = ProcessadorDeCsv.leitorCSV(nomeArquivo);
-			break;
-		}
-		case 2: {
-			pedidos = ProcessadorDeJson.leitorJson(nomeArquivo);
-			break;
-		}
-		case 3: {
-			pedidos = ProcessadorDeXml.leitorXml(nomeArquivo);
-			break;
-		}
-		default:
-			System.out.println("Opção inválida");
-		}
-    	
-    	System.out.println("Escolha o tipo de relatório que será impresso:\n1 - Relatório Sintético;\n2 - Relatório de Clientes Fiéis;\n3 - Relatório de Vendas por Categoria");
-    	opt = sc.nextInt();
-    	
-    	switch (opt) {
-		case 1: {
-			RelatorioSintetico relatorioSintetico = new RelatorioSintetico(pedidos);
-	        System.out.println(relatorioSintetico);
-	        break;
-		}
-		case 2: {
-			 RelatorioClientesFieis.relatorioClientesFieis(pedidos);
-			 break;
-		}
-		case 3: {
-			 RelatorioVendasCategoria.relatorioVendasPorCategoria(pedidos);
-			 break;
-		}
-		default:
-			System.out.println("Opção inválida");
-		}
-        
-        
-        sc.close();
+       
     }
     
     
