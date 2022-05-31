726456b29aa3e64453ff44f55ba8876dce66abb0
Larissa Labanca <ladyweigann@gmail.com>
2022-05-31 13:08:20 -0300

    Maior abrangência nos testes da classe Categoria de Vendas

--- src/main/java/br/com/alura/comex/relatorios/RelatorioVendasCategoria.java
+++ src/main/java/br/com/alura/comex/relatorios/RelatorioVendasCategoria.java
@@ -2,7 +2,6 @@ package br.com.alura.comex.relatorios;
 
 import java.math.BigDecimal;
 import java.util.List;
-import java.util.Map;
 import java.util.TreeMap;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
@@ -12,6 +11,13 @@ import br.com.alura.comex.Pedido;
 
 public class RelatorioVendasCategoria extends Relatorio {
 
+	private List<VendasCategoria> vendasPorCategoria;
+	
+	
+	public List<VendasCategoria> getVendasPorCategoria() {
+		return vendasPorCategoria;
+	}
+	
 	@Override
 	public void imprimirRelatorio(List<Pedido> pedidos, Consumer<String> impressoraDeRelatorio) {
 		if (pedidos == null || pedidos.isEmpty()) {
@@ -20,16 +26,50 @@ public class RelatorioVendasCategoria extends Relatorio {
 		
 		FormatarSaidaDeValores formatar = new FormatarSaidaDeValores();
 
-		Map<String, List<Pedido>> categorias = pedidos.stream()
-				.collect(Collectors.groupingBy(Pedido::getCategoria, TreeMap::new, Collectors.toList()));
-
-		categorias.forEach((categoria, pedido) -> {
-			impressoraDeRelatorio.accept("\n\nCATEGORIA: " + categoria + "\nQUANTIDADE VENDIDA: "
-					+ pedido.stream().mapToInt(Pedido::getQuantidade).sum() + "\nMONTANTE: "
-						+ formatar.formatarSaidaDeValores(pedido.stream().map(Pedido::getValorTotal).reduce(BigDecimal.ZERO, BigDecimal::add)));
+		vendasPorCategoria = pedidos.stream()
+				.collect(Collectors.groupingBy(Pedido::getCategoria, TreeMap::new, Collectors.toList()))
+				.entrySet().stream().map(entry -> {
+					return new VendasCategoria(entry.getKey(), 
+							entry.getValue().stream().mapToInt(Pedido::getQuantidade).sum(), 
+							entry.getValue().stream().map(Pedido::getValorTotal).reduce(BigDecimal.ZERO, BigDecimal::add));
+				}).toList();
+				
+			
+		vendasPorCategoria.stream().forEach(vendas -> {
+			impressoraDeRelatorio.accept("\n\nCATEGORIA: " + vendas.getCategoria() + "\nQUANTIDADE VENDIDA: "
+					+ vendas.getQuantidadeVendida() + "\nMONTANTE: "
+						+ formatar.formatarSaidaDeValores(vendas.getMontante()));
 
 		});
 
 	}
-
+	public class VendasCategoria {
+		
+		private String categoria;
+		private int quantidadeVendida;
+		private BigDecimal montante;
+		
+		
+		public VendasCategoria(String categoria, int quantidadeVendida, BigDecimal montante) {
+			this.categoria = categoria;
+			this.quantidadeVendida = quantidadeVendida;
+			this.montante = montante;
+		}
+		
+		
+		public String getCategoria() {
+			return categoria;
+		}
+		
+		public int getQuantidadeVendida() {
+			return quantidadeVendida;
+		}
+		
+		public BigDecimal getMontante() {
+			return montante;
+		}
+		
+	}
 }
+
+
