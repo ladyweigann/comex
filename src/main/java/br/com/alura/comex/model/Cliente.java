1909863cd699d79a872c813424550a1e9486cf77
Larissa Labanca <ladyweigann@gmail.com>
2022-06-07 21:54:47 -0300

    Classe para o Endereco do cliente criada com o Embeddable

--- src/main/java/br/com/alura/comex/model/Cliente.java
+++ src/main/java/br/com/alura/comex/model/Cliente.java
@@ -12,24 +12,16 @@ public class Cliente {
     private String nome;
     private String cpf;
     private String telefone;
-    //Endereço
-    private String rua;
-    private String numero;
-    private String complemento;
-    private String bairro;
-    private String cidade;
-    private String estado;
+    @Embedded
+    private Endereco endereco;
+
 
     public Cliente(String nome, String cpf, String telefone, String rua, String numero, String complemento, String bairro, String cidade, String estado) {
         this.nome = nome;
         this.cpf = cpf;
         this.telefone = telefone;
-        this.rua = rua;
-        this.numero = numero;
-        this.complemento = complemento;
-        this.bairro = bairro;
-        this.cidade = cidade;
-        this.estado = estado;
+        this.endereco = new Endereco(rua, numero, complemento, bairro, cidade, estado);
+
     }
 
     public Cliente() {
@@ -60,51 +52,7 @@ public class Cliente {
         this.telefone = telefone;
     }
 
-    public String getRua() {
-        return rua;
-    }
-
-    public void setRua(String rua) {
-        this.rua = rua;
-    }
-
-    public String getNumero() {
-        return numero;
-    }
-
-    public void setNumero(String numero) {
-        this.numero = numero;
-    }
-
-    public String getComplemento() {
-        return complemento;
-    }
-
-    public void setComplemento(String complemento) {
-        this.complemento = complemento;
-    }
-
-    public String getBairro() {
-        return bairro;
-    }
-
-    public void setBairro(String bairro) {
-        this.bairro = bairro;
-    }
-
-    public String getCidade() {
-        return cidade;
-    }
-
-    public void setCidade(String cidade) {
-        this.cidade = cidade;
-    }
-
-    public String getEstado() {
-        return estado;
-    }
-
-    public void setEstado(String estado) {
-        this.estado = estado;
+    public Endereco getEndereco() {
+        return endereco;
     }
 }
---
+++ src/main/java/br/com/alura/comex/model/Endereco.java
@@ -0,0 +1,51 @@
+package br.com.alura.comex.model;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Endereco {
+    //Endereço
+    private String rua;
+    private String numero;
+    private String complemento;
+    private String bairro;
+    private String cidade;
+    private String estado;
+
+    public Endereco(String rua, String numero, String complemento, String bairro, String cidade, String estado) {
+        this.rua = rua;
+        this.numero = numero;
+        this.complemento = complemento;
+        this.bairro = bairro;
+        this.cidade = cidade;
+        this.estado = estado;
+    }
+
+    public Endereco() {
+
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+}
