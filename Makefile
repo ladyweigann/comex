3e99587a7e3814ec23a2ff8d759ec1c43d783d4f
Larissa Labanca <ladyweigann@gmail.com>
2022-07-01 09:57:33 -0300

    Testes básicos no controller de categorias

--- Makefile
+++ Makefile
@@ -9,3 +9,17 @@ run: docker-image-build
 
 stop:
 	@ docker-compose down -v
+
+deploy: docker-image-build
+	@ docker login --username=  --password=$$(heroku auth:token) registry.heroku.com
+	@ docker build -t comex-api
+	@ docker image push registry.heroku.com/comex-api/web:1
+	@ make deploy_on_heroku IMAGE_ID=$$(docker image inspect registry.heroku.com/comex-api/web:1 -f {{.Id}})
+
+deploy_on_heroku:
+	@ curl -X PATCH \
+            -H "Authorization: Bearer $$(heroku auth:token)" \
+            -H "Content-Type: application/json" \
+            -H "Accept:application/vnd.heroku+json; version=3.docker-releases" \
+            -d '{ "updates": [{"type": "web",  "docker_image": "$(IMAGE_ID)"}] }' \
+            https://api.heroku.com/apps/comex-api/formation
--- src/test/java/br/com/alura/comex/controller/CategoriaControllerTest.java
+++ src/test/java/br/com/alura/comex/controller/CategoriaControllerTest.java
@@ -1,7 +1,61 @@
 package br.com.alura.comex.controller;
 
+import java.net.URI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("dev")
 class CategoriaControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
+
+    @Test
+    public void deveriaRetornar201Created() throws Exception {
+
+        URI uri = new URI("/api/categorias");
+        String json = "{\"nome\":\"Livros\"}";
+
+        mockMvc.perform(MockMvcRequestBuilders.post(uri).content(json).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isCreated());
+
+    }
+
+    @Test
+    public void deveriaBuscarCategoriaPeloId() throws Exception {
+
+        URI uri = new URI("/api/categorias/1");
+
+        mockMvc.perform(MockMvcRequestBuilders.get(uri).contentType(MediaType.APPLICATION_JSON))
+
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.nome").value("Livros"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.status").value("ATIVA"));
+
+    }
+
+    @Test
+    public void deveriaRetornarListaDePedidosPorCategoria() throws Exception {
+
+        URI uri = new URI("/api/categorias/pedidos");
+        mockMvc.perform(MockMvcRequestBuilders.get(uri))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON));
+
+    }
 
 
 }
